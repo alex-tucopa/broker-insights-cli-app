3d53201e26a538a4d2f9b47b858e4f1847e6922d
Alex Cooke <alex@tucopa.com>
2023-12-17 17:11:13 +0000

    Rename initialise database command

--- app/Commands/InitialiseDatabaseCommand.php
+++ app/Commands/InitialiseDatabaseCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'app:create-database-tables')]
+#[AsCommand(name: 'app:initialise-database')]
 class InitialiseDatabaseCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
